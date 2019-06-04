35e269fca16a6c0df20c6dde37ff7abf63a841dc
Shafreeck Sea <shafreeck@gmail.com>
2019-06-04 17:37:53 +0800

    return a std Result by main

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,9 +1,9 @@
-use std::io;
+use std::error::Error;
 
 use kiss::conf::Config;
 use kiss::server::Server;
 
-fn main() -> io::Result<()> {
+fn main() -> Result<(), Box<dyn Error>> {
     let c = Config {
         listen: "127.0.0.1:8804".to_string(),
     };
